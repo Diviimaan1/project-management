5425dd94d6e216db5ad7b2b5eaea7eaf512b2685
Diviimaan1 <diviimaan@gmail.com>
2026-01-17 15:47:40 +0530

    add healthcheck

--- src/app.js
+++ src/app.js
@@ -16,6 +16,11 @@ app.use(cors({
     allowedHeaders:["Content-Type","Authorization"]
 }))
 
+//adding the healthcheck routes
+import healthCheckRouter from "./routes/healthcheck.routes.js";
+
+app.use("/api/v1/healthcheck",healthCheckRouter);
+
 app.get("/",(req,res)=>{
    res.send("This is Holy SHiiii....."); 
 });
