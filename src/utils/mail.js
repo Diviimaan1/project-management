81d2fc72d64170a3c417150b57fa475b5532cc64
Diviimaan1 <diviimaan@gmail.com>
2026-01-18 02:29:25 +0530

    add send-email logic

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -1,4 +1,43 @@
 import Mailgen from "mailgen";
+import nodemailer from "nodemailer";
+
+const sendEmail = async (options) => {
+    const mailGenerator =  new Mailgen({
+        theme : "default",
+        product : {
+            name : "Task Manager",
+            link : "https://taskmanager.com"
+        }
+    })
+
+    const emailTextual = mailGenerator.generatePlaintext(options.mailgenContent)
+
+    const emailHtml = mailGenerator.generate(options.mailgenContent)
+
+    const transporter = nodemailer.createTransport({
+        host : process.env.MAILTRAP_SMTP_HOST,
+        port : process.env.MAILTRAP_SMTP_PORT,
+        auth : {
+            user : process.env.MAILTRAP_SMTP_USER,
+            pass : process.env.MAILTRAP_SMTP_PASS,
+        }
+    })
+
+    const mail = {
+        from : "mail.taskmanager@example.com",
+        to : options.email,
+        subject : options.subject,
+        text : emailTextual,
+        html : emailHtml,
+    } 
+
+    try {
+        await transporter.sendMail(mail)
+    } catch (error) {
+        console.error("Email service failed silently. Make sure to provide mailtrap credentials in .env file");
+        console.error("Error:",error);
+    }
+}
 
 const emailVerificationMailContent = ( username,verificationUrl ) => {
     return {
@@ -39,4 +78,5 @@ const forgotPasswordMailContent = ( username,passwordResetUrl ) => {
 export {
     emailVerificationMailContent,
     forgotPasswordMailContent,
+    sendEmail
 }
